38b466023ae19b8d9373257ee1acc72ef688f467
geovannatrz <144926724+geovannatrz@users.noreply.github.com>
2025-04-29 11:52:33 -0300

    AULA 29/04

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
-document.addEventListener('DOMContentLoaded' , function()
-{})
+document.addEventListener('DOMContentLoaded', function() {
+    const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade')
+    const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade')
+
+    botaoDeAcessibilidade.addEventListener('click', function (){
+        botaoDeAcessibilidade.classList.toggle('rotacao-botao')
+        opcoesDeAcessibilidade.classList.toggle('apresenta-lista')
+
+        const botaoSelecionado = botaoDeAcessibilidade.getAttribute('aria-expanded') === 'true';
+        botaoDeAcessibilidade.setAttribute('aria-expanded'), !botaoSelecionado
+    })
+
+    const aumentaFonteBotao = document.getElementById('aumentar-fonte');
+    const diminuiFonteBotao = document.getElementById('diminuir-fonte');
+
+    let tamanhoAtualFonte = 1
+    aumentaFonteBotao.addEventListener('click' function(){}
+})
+
+
+
+
